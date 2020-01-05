bff4032e6055d29f0d7d79a45269fa8548f6fd49
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-01-05 11:42:49 -0700

    set up store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,73 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios"
+
+let _api = axios.create({
+  baseURL: "//localhost:3000/api"
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    bugs: [],
+    activeBug: {},
+    notes: [],
+    activeNotes: []
   },
   mutations: {
+    setAllBugs(state, data) {
+      state.bugs = data
+    },
+    setActiveBug(state, bug) {
+      state.activeBug = bug
+    },
+    addBug(state, bug) {
+      state.bugs.push(bug)
+    },
+    setAllNotes(state, data) {
+      state.notes = data
+    },
+    setActiveNotes(state, notes) {
+      state.activeNotes = notes
+    },
+    addNote(state, note) {
+      state.notes.push(note)
+    }
   },
   actions: {
+    async getBugs({ commit, dispatch }) {
+      let res = await _api.get("bugs")
+      commit("setAllBugs", res.data)
+    },
+    async getBugById({ commit, dispatch }, id) {
+      let res = await _api.get("bugs/" + id)
+      commit("setActiveBug", res.data)
+    },
+    async createBug({ commit, dispatch }, bug) {
+      let res = await _api.post("bugs", bug)
+      commit("addBug", res.data)
+    },
+    async closeBug({ commit, dispatch }, id) {
+      await _api.delete("bugs/" + id)
+      dispatch("getBugs")
+    },
+    async getNotes({ commit, dispatch }) {
+      let res = await _api.get("")
+      commit("setAllNotes", res.data)
+    },
+    async getNotesByBugId({ commit, dispatch }, id) {
+      let res = await _api.get("bugs/" + id + "/notes")
+      commit("setActiveNotes", res.data)
+    },
+    async createNote({ commit, dispatch }, note) {
+      let res = await _api.post("notes", note)
+      commit("addNote", note)
+    },
+    async deleteNote({ commit, dispatch }, id) {
+      await _api.delete("notes/" + id)
+      dispatch("getNotes")
+    }
   },
   modules: {
   }
